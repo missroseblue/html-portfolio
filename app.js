601c84d9f250bc7423ac4b2d6187b31374f70fbe
missroseblue <missroseblue@gmail.com>
2021-10-03 18:37:19 -0700

    entered user prompts

--- app.js
+++ app.js
@@ -1,8 +1,62 @@
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+const fs = require("fs");
+const userInput = require("prompt-sync");
+const [userName, email, title, description, license, installation, tests, usage, contributing] = userInput();
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem)); 
+let userName = prompt("What is your GitHub user name?");
+let email = propmt("What is your email address?");
+let title = prompt("What is the title of your project?");
+let description = prompt("Please write a short descrption of your project.");
+let license = prompt("What license should you project have?");
+let installation = prompt("What command should be run to install dependencies?");
+let tests = prompt("What command should be run to run tests?");
+let usage = prompt("What does the user need to know about using the repository?");
+let contributing = prompt("What does the user need to know about contributing to the repository?");
+
+
+// const [title, description, tableOfContents, installation, usage, license, contributing, tests, questions] = profileDataArgs;
+
+const generateReadMe = (userInput) => {
+  return `
+  # ${title}
+
+  ## Description
+  ${description}
+
+  ## Table of Contents
+  [Title](#title)
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributing](#contributing)
+  [Tests](#tests)
+  [Questions](#questions)
+
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## License
+  ${license}
+
+  ## Contributing
+  ${contributing}
+
+  ## Tests
+  ${tests}
+
+  ## Questions
+  GitHub Username: ${userName}
+  Email: ${email}
+
+  `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile("README.MD", generateReadMe(), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("README file is complete! Check out README.MD to see the output!");
+});
