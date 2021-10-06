f265e92c086102b17d8455597a7104892da545d3
missroseblue <missroseblue@gmail.com>
2021-10-05 21:48:00 -0700

    added inquirer

--- app.js
+++ app.js
@@ -1,62 +1,114 @@
-const fs = require("fs");
-const userInput = require("prompt-sync");
-const [userName, email, title, description, license, installation, tests, usage, contributing] = userInput();
+// const fs = require("fs");
+const inquirer = require("inquirer");
+// const userInput = require("prompt-sync");
 
-let userName = prompt("What is your GitHub user name?");
-let email = propmt("What is your email address?");
-let title = prompt("What is the title of your project?");
-let description = prompt("Please write a short descrption of your project.");
-let license = prompt("What license should you project have?");
-let installation = prompt("What command should be run to install dependencies?");
-let tests = prompt("What command should be run to run tests?");
-let usage = prompt("What does the user need to know about using the repository?");
-let contributing = prompt("What does the user need to know about contributing to the repository?");
+inquirer.prompt([
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub user name?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short descrption of your project."
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What license should you project have?"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repository?"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "What does the user need to know about contributing to the repository?"
+  },
+])
+.then(answers => console.log(answers));
+// .then (function(userInput) {
+//   const 
+// })
+
+// const generateReadMe = (userInput) => {
+//   return `
+//   # ${title}
+
+//   ## Description
+//   ${description}
+
+//   ## Table of Contents
+//   [Title](#title)
+//   [Description](#description)
+//   [Installation](#installation)
+//   [Usage](#usage)
+//   [License](#license)
+//   [Contributing](#contributing)
+//   [Tests](#tests)
+//   [Questions](#questions)
 
 
-// const [title, description, tableOfContents, installation, usage, license, contributing, tests, questions] = profileDataArgs;
+//   ## Installation
+//   ${installation}
 
-const generateReadMe = (userInput) => {
-  return `
-  # ${title}
+//   ## Usage
+//   ${usage}
 
-  ## Description
-  ${description}
+//   ## License
+//   ${license}
 
-  ## Table of Contents
-  [Title](#title)
-  [Description](#description)
-  [Installation](#installation)
-  [Usage](#usage)
-  [License](#license)
-  [Contributing](#contributing)
-  [Tests](#tests)
-  [Questions](#questions)
+//   ## Contributing
+//   ${contributing}
 
+//   ## Tests
+//   ${tests}
 
-  ## Installation
-  ${installation}
+//   ## Questions
+//   GitHub Username: ${userName}
+//   Email: ${email}
 
-  ## Usage
-  ${usage}
+//   `;
+// };
 
-  ## License
-  ${license}
+// fs.writeFile("README.MD", generateReadMe(), (err) => {
+//   if (err) throw new Error(err);
 
-  ## Contributing
-  ${contributing}
+//   console.log(
+//     "README file is complete! Check out README.MD to see the output!"
+//   );
+// });
 
-  ## Tests
-  ${tests}
-
-  ## Questions
-  GitHub Username: ${userName}
-  Email: ${email}
-
-  `;
-};
-
-fs.writeFile("README.MD", generateReadMe(), (err) => {
-  if (err) throw new Error(err);
-
-  console.log("README file is complete! Check out README.MD to see the output!");
-});
+// let userName = prompt("What is your GitHub user name?");
+// let email = propmt("What is your email address?");
+// let title = prompt("What is the title of your project?");
+// let description = prompt("Please write a short descrption of your project.");
+// let license = prompt("What license should you project have?");
+// let installation = prompt("What command should be run to install dependencies?");
+// let tests = prompt("What command should be run to run tests?");
+// let usage = prompt("What does the user need to know about using the repository?");
+// let contributing = prompt("What does the user need to know about contributing to the repository?");
